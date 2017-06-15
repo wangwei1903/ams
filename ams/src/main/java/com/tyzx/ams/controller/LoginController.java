50859fa45dcd51253b693b4cc69412a1779967ac
wangwei1903 <1219389299@qq.com>
2017-06-15 17:55:32 +0800

    第二次修改

--- ams/src/main/java/com/tyzx/ams/controller/LoginController.java
+++ ams/src/main/java/com/tyzx/ams/controller/LoginController.java
@@ -3,6 +3,8 @@
  */
 package com.tyzx.ams.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ import com.tyzx.ams.util.MD5Util;
 @RequestMapping("/order")
 public class LoginController {
 
+	private Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	
 	@Autowired
     private UserService userservice;
 	
@@ -31,7 +35,8 @@ public class LoginController {
 	@RequestMapping(value="/login/{name}/{password}",method=RequestMethod.POST)
 	@ResponseBody
 	public String login(@PathVariable("name") String name,@PathVariable("password") String password) {
-		System.out.println(name+"<>"+password);
+		LOGGER.debug("login");
+		LOGGER.info(name+"<<<>>>"+password);
 		JSONObject object = new JSONObject();
 		String MD5Password = MD5Util.getMD5(password);
 		User user = new User(MD5Password, name);
@@ -42,8 +47,15 @@ public class LoginController {
 			object.put("code","1");
 			object.put("user", user);
 		}
-		System.out.println(object.toJSONString());
+		LOGGER.info(object.toJSONString());
 		return object.toJSONString();
+	}
+	
+	@RequestMapping(value = "/postLog",method=RequestMethod.POST)
+	@ResponseBody
+	public String postLog(){
+		LOGGER.info("postLog");
+		return "222";
 	}
 	
 }
